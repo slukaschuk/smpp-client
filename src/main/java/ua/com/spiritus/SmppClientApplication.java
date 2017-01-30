7e2fbad8a7fb9777398b610c5e08cf97d0bc1cd9
Sergey Lukaschuk <sergey.lukaschuk@gmail.com>
2017-01-30 10:37:41 +0200

    create project

---
+++ src/main/java/ua/com/spiritus/SmppClientApplication.java
@@ -0,0 +1,12 @@
+package ua.com.spiritus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmppClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmppClientApplication.class, args);
+	}
+}
